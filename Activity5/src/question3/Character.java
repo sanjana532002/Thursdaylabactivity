bd4e45d0dfc07b357d083bf28784698e0ceef825
Sanjana Mane <Sanjana Mane@LAPTOP-EC6U86VK>
2022-12-15 18:58:26 +0530

    Activity 5

---
+++ Activity5/src/question3/Character.java
@@ -0,0 +1,46 @@
+// Author: Sanjana Mane
+
+package question3;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Character {
+public static void main(String[] args) {
+	{
+		try
+		{
+			int lines=0,chars=0,words=0;
+			int code=0;
+			FileInputStream fis = new FileInputStream("prajwalgadakh/desktop/char.txt");// accesing files
+			while(fis.available()!=0)
+			{
+				code = fis.read();
+				if(code!=10)
+				chars++;
+				if(code==32)
+				words++;
+				if(code==13)
+				{
+					lines++;
+					words++;
+				}
+			}
+			System.out.println("No.of characters = "+chars);
+			System.out.println("No.of words = "+(words+1));
+			System.out.println("No.of lines = "+(lines+1));
+			fis.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("Cannot find the  file");
+		}
+		catch(IOException i)
+		{
+			System.out.println("file not found ");
+		}
+	}
+}
+
+	}
